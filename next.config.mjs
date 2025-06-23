ac2997e969cd9c28907fff1afeb1711b5e333c02
balavenk <balavenk@gmail.com>
2025-06-23 12:22:53 -0400

    out folder created

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +24,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
+
+
+// export default nextConfig
