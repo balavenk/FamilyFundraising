d09a9e7d95db8551b4826e37cee898173d4005e3
balavenk <balavenk@gmail.com>
2025-06-28 00:48:44 -0400

    build

--- next.config.mjs
+++ next.config.mjs
@@ -11,22 +11,13 @@
 //   },
 // }
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-};
+const isProd = process.env.NODE_ENV === 'production';
 
-export default nextConfig;
+export default {
+  output: 'export',
+  basePath: isProd ? '/familytreee' : '',
+  assetPrefix: isProd ? '/familytree/' : '',
+};
 
 
 // export default nextConfig
